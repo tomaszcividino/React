4ffc85f82f3fa64d42b8949a69a66a81d35bcb59
tomaszcividino <tomaszcividino@gmail.com>
2021-08-14 11:30:11 +0200

    Init Commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import styles from './App.scss';
+
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1>My first React app</h1>
-      </div>
+      <main className={styles.component}>
+        <h1 className={styles.title}>My first React app</h1>
+        <h2 className={styles.subtitle}>Hello world!</h2>
+      </main>
     )
   }
 }
+
+ 
 
 export default App;
